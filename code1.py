bb2fabd035c49d2332c25376e381d4de29eb415e
Ho1 <wjdgh62@gmail.com>
2023-01-09 17:19:10 +0900

    iris data2csvc 2

--- code1.py
+++ code1.py
@@ -1,15 +1,14 @@
-
 from sklearn.datasets import load_iris
 import pandas as pd
 import numpy as np
 
-Iris = load_iris()
+iris = load_iris()
 
-Iris_Data = pd.DataFrame(data=np.c_[Iris['data'], Iris['target']], columns=Iris['feature_names']+['target'])
-Iris_Data['target'] = Iris_Data['target'].map({0: "setosa", 1:"versicolor", 2:"virginica"})
-Iris_Data.to_csv('iris_classification.csv')
-print(iris_data)
+iris_Data = pd.DataFrame(data=np.c_[iris['data'], iris['target']], columns=iris['feature_names']+['target'])
+iris_Data['target'] = iris_Data['target'].map({0: "setosa", 1:"versicolor", 2:"virginica"})
+iris_Data.to_csv('iris_classification.csv')
+print(iris_Data)
 
-X_Data = Iris_Data.iloc[:, :-1]
-Y_Data = Iris_Data.iloc[:, [-1]]
+X_Data = iris_Data.iloc[:, :-1]
+Y_Data = iris_Data.iloc[:, [-1]]
 
